c81af8b75c7f015cace02bd1522a4c6bbe9505a3
ossamakada75 <ossamakada75@gmail.com>
2023-08-16 08:58:14 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,39 +1,16 @@
+#include "function_pointers.h"
 #include <stdio.h>
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
 
-void print_name_default(char *name) {
-    printf("Name: %s\n", name);
-}
-
-void print_name_uppercase(char *name) {
-    for (int i = 0; name[i]; i++) {
-        putchar(toupper(name[i]));
-    }
-    putchar('\n');
-}
-
-void print_name_lowercase(char *name) {
-    for (int i = 0; name[i]; i++) {
-        putchar(tolower(name[i]));
-    }
-    putchar('\n');
-}
-
-void print_name(char *name, void (*f)(char *)) {
-    f(name);
-}
-
-int main() {
-    char name[] = "John Doe";
-
-    printf("Default: ");
-    print_name(name, print_name_default);
-
-    printf("Uppercase: ");
-    print_name(name, print_name_uppercase);
-
-    printf("Lowercase: ");
-    print_name(name, print_name_lowercase);
-
-    return 0;
+	f(name);
 }
 
